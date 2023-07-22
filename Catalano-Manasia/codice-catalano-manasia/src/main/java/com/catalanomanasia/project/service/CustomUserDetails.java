45896a61b96f91e04d0c974032746cbf9057c404
mana2898 <73307208+mana2898@users.noreply.github.com>
2023-07-22 17:58:57 +0200

    Update CustomUserDetails.java

--- Catalano-Manasia/codice-catalano-manasia/src/main/java/com/catalanomanasia/project/service/CustomUserDetails.java
+++ Catalano-Manasia/codice-catalano-manasia/src/main/java/com/catalanomanasia/project/service/CustomUserDetails.java
@@ -21,6 +21,20 @@ public class CustomUserDetails implements UserDetails {
         this.user = user;
     }
 
+
+    /*La classe CustomUserDetails sembra essere una classe personalizzata che implementa l'interfaccia UserDetails 
+    di Spring Security. Questa classe viene utilizzata per fornire informazioni dettagliate sull'utente al sistema 
+    di autenticazione di Spring Security.
+La classe CustomUserDetails ha un campo user di tipo User, che rappresenta l'oggetto utente associato a 
+    questa istanza di CustomUserDetails. Il metodo getUser() restituisce l'oggetto User associato.
+Il costruttore CustomUserDetails(User user) viene utilizzato per creare un'istanza di CustomUserDetails a 
+    partire da un oggetto User. Questo consente di incapsulare le informazioni sull'utente all'interno di 
+    un oggetto CustomUserDetails che può essere utilizzato dal sistema di autenticazione di Spring Security.
+L'implementazione specifica dei metodi dell'interfaccia UserDetails all'interno della classe CustomUserDetails 
+    non è mostrata nel codice fornito, ma di solito include la restituzione delle informazioni sull'utente 
+    come nome utente, password, autorizzazioni e altri dettagli richiesti dal sistema di autenticazione di Spring Security.
+I risultati della ricerca forniscono ulteriori informazioni su come creare un oggetto UserDetails personalizzato 
+    in Spring Security, inclusi esempi di implementazione e guide dettagliate. */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities(){
         Set<Role> roles = new HashSet<>();
