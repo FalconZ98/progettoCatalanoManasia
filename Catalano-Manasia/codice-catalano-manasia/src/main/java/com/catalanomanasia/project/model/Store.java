c74db3746fe4f92074a5aa0294c4f273b3807085
mana2898 <73307208+mana2898@users.noreply.github.com>
2023-07-22 16:04:36 +0200

    Update Store.java

--- Catalano-Manasia/codice-catalano-manasia/src/main/java/com/catalanomanasia/project/model/Store.java
+++ Catalano-Manasia/codice-catalano-manasia/src/main/java/com/catalanomanasia/project/model/Store.java
@@ -51,10 +51,22 @@ public class Store {
         return Objects.equals(id, store.id) && Objects.equals(name, store.name);
     }
 
+    /* Il codice fornito include l'annotazione @Override, che indica che il metodo equals() sta sovrascrivendo un metodo 
+    della classe genitore. Il metodo equals() è un metodo speciale di Java che viene utilizzato per confrontare due oggetti e 
+    determinare se sono uguali o meno. Il metodo equals() ereditato dalla classe Object di Java confronta gli oggetti in base 
+    all'indirizzo di memoria, il che significa che due oggetti con gli stessi valori ma in posizioni di memoria diverse 
+    sarebbero considerati diversi. */
+
+
     @Override
     public int hashCode() {
         return Objects.hash(id, name);
     }
+
+    /*Nel codice fornito, il metodo hashCode() viene sovrascritto per calcolare il codice hash dell'oggetto di tipo Store 
+    in base ai suoi campi id e name. Viene utilizzato il metodo statico Objects.hash() per calcolare il codice hash combinando 
+    i valori dei campi id e name. Questo metodo restituisce un valore hash intero che rappresenta l'oggetto Store.*/
+
 
     @Override
     public String toString() {
